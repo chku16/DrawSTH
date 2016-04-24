95204cae0dda1ec18c4f874db9a4ff9442b68356
chku16 <yysck1992@hotmail.com>
2016-04-24 16:59:11 +0800

    Fixed wrong route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('painter', { title: 'Express' });
+  res.render('paint', { title: 'Express' });
 });
 
 module.exports = router;
